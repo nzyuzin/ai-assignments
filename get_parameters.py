9f511cbca307fcbbc944f252719eaf196bf7904a
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-11-07 13:03:04 +0400

    Add program for random parameters generation

---
+++ get_parameters.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import random
+import sys
+
+def printRandomParameters(n):
+  for _ in range(n):
+    first = random.randrange(-10, 1)
+    second = round(random.uniform(1, 2), 2)
+    thirdNumber = random.randrange(0,1)
+    third = True if thirdNumber == 1 else False
+
+    print(str(first) + ' ' + str(second) + ' ' + str(third).lower())
+
+if __name__ == "__main__":
+  printRandomParameters(int(sys.argv[1]))
